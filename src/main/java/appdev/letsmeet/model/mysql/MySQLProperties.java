e74a2c2f0d96bce2c6db41accff3ce9a98770de4
Owner <Owner@PCPower-100413>
2016-01-10 06:26:20 +0200

    Merge origin/Redis
    Conflicts:
    	src/main/java/appdev/letsmeet/control/rest/application/HomeController.java
    	src/main/java/appdev/letsmeet/model/LetsMeet.java
    	src/main/java/appdev/letsmeet/model/mysql/MySQLHandler.java

--- src/main/java/appdev/letsmeet/model/mysql/MySQLProperties.java
+++ src/main/java/appdev/letsmeet/model/mysql/MySQLProperties.java
@@ -14,8 +14,8 @@ public class MySQLProperties {
     public static final String serverName = "localhost";
     public static final String dBName = "jdbc/lets_meet";
     public static final int port = 3306;
-    public static final String username = "root";
-    public static final String password = "root";
+    public static final String username = "admin";
+    public static final String password = "admin";
     public static final String driver = "com.mysql.jdbc.Driver";
     public static final String url = "jdbc:mysql://localhost:3306/lets_meet";
 }
